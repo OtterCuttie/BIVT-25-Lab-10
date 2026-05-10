f4dd1a8bd0b5bd9be6f845aa790391e4600f9102
CuttiePie <144522961+OtterCuttie@users.noreply.github.com>
2026-05-10 20:26:41 +0300

    Add files via upload

---
+++ Lab9/Blue/Task2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab9.Blue
+{
+    public class Task2 : Blue
+    {
+        private string _output ="";
+        public string Output { get; private set; }
+
+        private string _combo;
+        public string Combo { get; private set; }
+
+        public Task2(string input, string combo) : base(input)
+        {
+            Combo = combo;
+        }
+        public override void Review()
+        {
+            
+            string[] words = _input.Split(new char[] { ' ' }, StringSplitOptions.None);
+
+            foreach (string word in words)
+            {
+                if (_combo == null) return;
+                if (!word.Contains(_combo))
+                {
+                    if (_output.Length == 0)
+                    {
+                        _output = word;
+                    }
+                    else _output += ' ' + word;
+                }
+                else
+                {
+
+                    char[] punctuationMarks = { '.', ',', '!', '?', ';', ':' };
+                    if (word.Contains('\"'))
+                    {
+                        if (word.Any(c => punctuationMarks.Contains(c)))
+                        {
+                            _output += " \"\""+word[^1];
+                        }
+                        else if (_output.Length == 0)
+                            _output = "\"\"";
+                        else
+                            _output += " \"\"";
+                    }
+                    else if (word.Any(c => punctuationMarks.Contains(c))){
+                        _output += word[^1];
+                    }
+
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return _output;
+        }
+    }
+}
